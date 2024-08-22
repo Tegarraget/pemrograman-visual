c9dd47d62c334f2cbaf083c3d887fa8ef84c66f4
Tegar Farhandi Syarief <113575601+Tegarraget@users.noreply.github.com>
2024-08-22 10:37:33 +0700

    Add files via upload

---
+++ TugasPakMujib1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugaspakmujib1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class TugasPakMujib1 {
+
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        modul6 modul=new modul6();
+        modul.setVisible(true);    }
+    
+}
